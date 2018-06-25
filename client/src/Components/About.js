34bc1162e13f63ebe80d809667edbd9f41aba628
hayz999 <hlzulkoski@gmail.com>
2018-06-25 16:13:12 -0600

    added sponsor logos

--- client/src/Components/About.js
+++ client/src/Components/About.js
@@ -8,7 +8,11 @@ export default class About extends Component {
                 <p>Bettership is an app that does things.  It helps students and is super great. </p>
                 <div>
                 <h2>Sponsor Companies</h2>
-                    {/* logos here */}
+                <img  src='./assets/art-museum.jpg' alt='Art Museum' />
+                <img  src='./assets/Bp-logo.jpg' alt='BP' />
+                <img  src='./assets/fb-logo.png' alt='FB' />
+                <img  src='./assets/hospital.jpg' alt='Childrens Hospital' />
+                <img  src='./assets/red-cross.png' alt='Red Cross' />
                 </div>
             </div>
         );
