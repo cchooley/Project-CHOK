8f91663c3d07912cc5a3c43b15534a5bbf32dd88
hayz999 <hlzulkoski@gmail.com>
2018-06-27 15:50:53 -0600

    cleaned up code format

--- client/src/Components/AppForm.js
+++ client/src/Components/AppForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import { Form, TextArea, Button, Segment } from 'semantic-ui-react'
 
 const options = [
@@ -10,22 +10,33 @@ export default class AppForm extends Component {
   state = {}
 
   handleChange = (e, { value }) => this.setState({ value })
+  
   render() {
     const { value } = this.state
     return (
       <Segment>
         <Form>
-          <Form.Input fluid label='First name' placeholder='First name' />
-          <Form.Input fluid label='Last name' placeholder='Last name' />
-          <Form.Input fluid label='Address' placeholder='Address' />
-          <Form.Input fluid label='Phone number' placeholder='Phone number' />
-          <Form.Input fluid label='Email' placeholder='Email' />
-          <Form.Input fluid label='Age' placeholder='Age' />
-          <Form.Select fluid label='Gender' options={options} placeholder='Gender' />
-          <Form.Field control={TextArea} label='About' placeholder='Tell us more about you...' />
+          <Form.Input fluid label='First name' 
+                      placeholder='First name' />
+          <Form.Input fluid label='Last name' 
+                      placeholder='Last name' />
+          <Form.Input fluid label='Address' 
+                      placeholder='Address' />
+          <Form.Input fluid label='Phone number' 
+                      placeholder='Phone number' />
+          <Form.Input fluid label='Email' 
+                      placeholder='Email' />
+          <Form.Input fluid label='Age' 
+                      placeholder='Age' />
+          <Form.Select  fluid label='Gender' 
+                        options={options} 
+                        placeholder='Gender' />
+          <Form.Field control={TextArea} 
+                      label='About' 
+                      placeholder='Tell us more about you...' />
           <Form.Field control={Button}>Submit</Form.Field>
         </Form>
       </Segment>
-    );
+    )
   }
 }
