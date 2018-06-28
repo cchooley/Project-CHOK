2dada17cbc5f9e2ccb7b15c94426ea8f85938e32
hayz999 <hlzulkoski@gmail.com>
2018-06-28 09:47:19 -0600

    maping through profiles just for now

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,16 +1,22 @@
 import React, { Component } from 'react';
-import { Header, Image, Segment, Button } from 'semantic-ui-react'
+import { Image, Segment, Button } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
 
 export default class Profile extends Component {
 
   render() {
-    
+    const profiles = this.props.data.map(profile => {
+      return (
+        <Segment>
+          <Image src={profile.picture} size='medium'/>
+          <h1>{profile.name}</h1>
+          <h4>{profile.school}</h4>
+          <Link to='/internships'><Button size='massive'>Apply to Internship</Button></Link>
+        </Segment>
+      )
+    })
     return (
-      <Segment>
-        <Image/>
-        <Header></Header>
-        <Button size='massive'>Apply to Internship</Button>
-      </Segment>
+      <div>{profiles}</div>
     );
   }
 }
